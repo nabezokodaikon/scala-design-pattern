a50be4923b544c386dd9b8bd300dc6adc6bd3dcb
nabezokodaikon <nabezoko.daikon@gmail.com>
2015-06-06 03:26:25 +0900

    Singleton パターン実装

---
+++ src/main/scala/scaladf/creational/SingletonPattern.scala
@@ -0,0 +1,10 @@
+package scaladf.creational
+
+object Singleton {
+  val x = 1
+  val y = 2
+
+  override def toString() = {
+    "x = %d, y = %d".format(x, y)
+  }
+}
---
+++ src/test/scala/scaladf/creational/SingletonPatternTest.scala
@@ -0,0 +1,10 @@
+package scaladf.creational
+
+import org.scalatest.FunSuite
+
+class SingletonPatternTest extends FunSuite {
+
+  test("Singleton") {
+    println(Singleton.toString())
+  }
+}
